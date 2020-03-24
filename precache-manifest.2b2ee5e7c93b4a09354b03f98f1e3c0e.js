66844438537ddf0ebe4758752a4991098c741e4c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-24 20:47:07 +0000

    Deploy casseds2/Corona-HQ to github.com/casseds2/Corona-HQ.git:gh-pages

---
+++ precache-manifest.2b2ee5e7c93b4a09354b03f98f1e3c0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292fa92b62be33e5fd2ac63a1d0c7b04",
+    "url": "/Corona-HQ/css/main.css"
+  },
+  {
+    "revision": "aca57fdcc30f4e4f93acd404783dda1a",
+    "url": "/Corona-HQ/index.html"
+  },
+  {
+    "revision": "05a3a5680decfaa2d8bf",
+    "url": "/Corona-HQ/js/app.43ec3ce9.js"
+  },
+  {
+    "revision": "25280eef56c7419c0568",
+    "url": "/Corona-HQ/js/chunk-vendors.df5b30b1.js"
+  },
+  {
+    "revision": "41c9066bbca6eec251f1ff918d2eca5c",
+    "url": "/Corona-HQ/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Corona-HQ/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Corona-HQ/precache-manifest.2b2ee5e7c93b4a09354b03f98f1e3c0e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "corona-hq"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
